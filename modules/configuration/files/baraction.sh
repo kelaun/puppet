558e6d94c96defb1f4aa43fb6d094c6821cced70
Kelaun <kelaun@kelaun.be>
2020-08-22 18:54:49 +0200

    Adding CPU temperature

--- modules/configuration/files/baraction.sh
+++ modules/configuration/files/baraction.sh
@@ -6,14 +6,14 @@ while :; do
 # UPDATES
 AMOUNT="$(yay -Qu | wc -l)"
 RESULT="$(case $AMOUNT in
-  1) echo "$AMOUNT update" ;;
-  0) echo up-to-date ;; 
-  *) echo "$AMOUNT updates" ;; 
+	1) echo "$AMOUNT update" ;;
+	0) echo up-to-date ;;
+	*) echo "$AMOUNT updates" ;;
 esac)"
 
 # NETWORK
 NETSTAT="$(nmcli -p | grep wlan0 | head -1 | awk '{ print $2 }')"
-NETNAME="$(if [ $NETSTAT == "connected" ]; then echo "$(nmcli -p | grep wlan | head -1 | awk '{ print $4 }')"; else echo $NETSTAT; fi)"
+NETNAME="$(if [ $NETSTAT == "connected" ]; then echo "$(nmcli -p | grep wlan0 | head -1 | awk '{ print $4 }')"; else echo $NETSTAT; fi)"
 
 # VOLUME
 VOLUME="$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')%"
@@ -21,8 +21,18 @@ VOLUME="$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 
 # DATE
 TODAY="$(date "+%d %B, %H:%M:%S")"
 
+# TEMPERATURE
+TEMPS=$(sensors | grep Core | awk '{ print $3 }' | awk -F'.' '{ print $1 }' | sed 's/+//')
+MEDIAN=
+CORES=$(sensors | grep Core | wc -l)
+
+for TEMP in $TEMPS; do
+	MEDIAN=$(($MEDIAN + $TEMP))
+done
+MEDIAN="$(($MEDIAN/$CORES))°C"
 
 # Final bar
-echo "${RESULT}   ${NETNAME}   ${VOLUME}   ${TODAY}"
+echo "${MEDIAN}   ${RESULT}   ${NETNAME}   ${VOLUME}   ${TODAY}"
 sleep 1
+
 done
