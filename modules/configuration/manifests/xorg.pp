c77a11e1650d49a01122719019c95842ae05a3e4
Kelaun <kevin@kelaun.be>
2020-08-29 14:45:40 +0200

    Adding gpd specific config

--- modules/configuration/manifests/xorg.pp
+++ modules/configuration/manifests/xorg.pp
@@ -5,4 +5,20 @@
 # @example
 #   include configuration::xorg
 class configuration::xorg {
+  file { 'display':
+    path   => '/etc/X11/xorg.conf.d/10-display.conf',
+    ensure => file,
+    group  => 'root',
+    mode   => '0644',
+    owner  => 'root',
+    source => 'puppet:///modules/configuration/10-display.conf',
+  }
+  file { 'touchscreen':
+    path   => '/etc/X11/xorg.conf.d/99-touchscreen.conf',
+    ensure => file,
+    group  => 'root',
+    mode   => '0644',
+    owner  => 'root',
+    source => 'puppet:///modules/configuration/99-touchscreen.conf',
+  }
 }
