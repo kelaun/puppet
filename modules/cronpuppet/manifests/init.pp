70f11d65ec434339f1e8dab09d6ccab27bd5cb49
Kelaun <kevin@kelaun.be>
2020-09-09 10:49:39 +0200

    Changed sources to use new scripts repository

--- modules/cronpuppet/manifests/init.pp
+++ modules/cronpuppet/manifests/init.pp
@@ -8,7 +8,7 @@ class cronpuppet {
   file { 'post-hook':
     ensure => 'file',
     path   => '/etc/puppet/.git/hooks/post-merge',
-    source => 'puppet:///modules/cronpuppet/post-merge',
+    source => 'https://raw.githubusercontent.com/kelaun/scripts/desktop/puppet/post-merge',
     mode   => '0755',
     owner  => 'root',
     group  => 'root',
